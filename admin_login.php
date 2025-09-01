e6a8103bf90740f6e92364ba7c80ca12cfe5279f
Faruk Khan <faruk.khan.cse@gmail.com>
2025-09-01 22:17:10 +0530

    backend removed

--- admin_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if ($_POST['username'] === 'admin' && $_POST['password'] === 'Nucleon@2k25') {
-    $_SESSION['admin'] = true;
-    echo json_encode(["success" => true]);
-} else {
-    echo json_encode(["success" => false]);
-}
-?>
